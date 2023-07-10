0394b59faa56a1f7221daa9b18d0bf8d940e5df2
jonathans199c <jonathasa@city-furniture.com>
2023-07-10 16:20:13 -0400

    updated items preventing the next build

--- src/components/LoadGreenTurtle.tsx
+++ src/components/LoadGreenTurtle.tsx
@@ -9,11 +9,12 @@ export function GreenTurtleModel({ ...props }) {
   const group = useRef()
   const { nodes, materials, animations }: any = useGLTF('/assets/greenseaturtle/scene.gltf')
   const { actions, names }: any = useAnimations(animations, group)
-  if (actions && names) {
-    useEffect(() => {
+
+  useEffect(() => {
+    if (actions && names) {
       actions[names[0]].reset().fadeIn(0.5).play()
-    }, [actions])
-  }
+    }
+  }, [actions])
 
   return (
     <group ref={group as any} {...props} dispose={null}>
@@ -22,14 +23,33 @@ export function GreenTurtleModel({ ...props }) {
           <group name='green_016_round5changes_johnsonfbx' rotation={[Math.PI / 2, 0, 0]}>
             <group name='Object_2'>
               <group name='RootNode'>
-                <group name='green_juvenilearmature' position={[-2.27, 2.99, -2.19]} rotation={[-Math.PI / 2, 0, -3.12]} scale={100}>
+                <group
+                  name='green_juvenilearmature'
+                  position={[-2.27, 2.99, -2.19]}
+                  rotation={[-Math.PI / 2, 0, -3.12]}
+                  scale={100}>
                   <group name='Object_5'>
                     <primitive object={nodes._rootJoint} />
                     <group name='Object_39' rotation={[-Math.PI / 2, 0, 0]} scale={100} />
                     <group name='Object_42' rotation={[-Math.PI / 2, 0, 0]} scale={100} />
-                    <skinnedMesh name='Object_40' geometry={nodes.Object_40.geometry} material={materials.greeneye} skeleton={nodes.Object_40.skeleton} />
-                    <skinnedMesh name='Object_41' geometry={nodes.Object_41.geometry} material={materials.greeneyeouter} skeleton={nodes.Object_41.skeleton} />
-                    <skinnedMesh name='Object_43' geometry={nodes.Object_43.geometry} material={materials.greenbody} skeleton={nodes.Object_43.skeleton} />
+                    <skinnedMesh
+                      name='Object_40'
+                      geometry={nodes.Object_40.geometry}
+                      material={materials.greeneye}
+                      skeleton={nodes.Object_40.skeleton}
+                    />
+                    <skinnedMesh
+                      name='Object_41'
+                      geometry={nodes.Object_41.geometry}
+                      material={materials.greeneyeouter}
+                      skeleton={nodes.Object_41.skeleton}
+                    />
+                    <skinnedMesh
+                      name='Object_43'
+                      geometry={nodes.Object_43.geometry}
+                      material={materials.greenbody}
+                      skeleton={nodes.Object_43.skeleton}
+                    />
                   </group>
                 </group>
                 <group name='green_juvenileeye' rotation={[-Math.PI / 2, 0, 0]} scale={100} />
