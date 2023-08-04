9dbf62c845613f4267b14fb96b1177f68cddceb7
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2023-08-03 20:37:22 -0400

    our beginnings added

---
+++ src/components/ourBeginning/ourBeginning.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { ContentModel } from '../ContentModel/ContentModel'
+import './styles.css'
+
+const OurBeginning = () => {
+  const [visible, setVisible] = React.useState(false)
+  const modelContent = (
+    <p>
+      Like so many things in life, it just began, without looking for it or even knowing about it.
+      <br />
+      On an early July morning in 2007, Siouxzen WhiteCloud made her way through the humid darkness to a Deerfield Beach to teach yoga. As she crossed A1A a sight awaited her. It was not a new sight, it had happened a thousand times before, but this time the right person was looking... <br />
+      Stretched in front of her, covering the four-lane beach road, lifeless in the darkness of the fading night, were hundreds of run over baby loggerhead sea turtles. Following their perfect instinct to go towards the light of the moon and the stars reflecting on the sea, they had poured into A1A
+      pulled west by its glaring street lights and were killed at just a few minutes old. <br />
+      That day Siouxzen and her husband, Richard, called every sea turtle organization in the country, looking for help, guidance, reasons, answers. And that night they took their three-year-old daughter, Teakahla, and went out on the beach and sat next to a loggerhead sea turtle nest. Of the
+      thousands in Broward County, they could cover at least one. Scheduled to move to New Mexico in two days, they changed their life plans and decided they could not leave Florida until the sea turtle hatchlings of Broward County were safe. <br />
+      Richard launched into intense research of the coastal lighting problems and infractions, of the shortcomings of the Broward County contractor (under previous mismanagement) assigned to oversee the sea turtle conservation program, and started a grassroots group of volunteers who were willing to
+      not sleep. It took several more summers of walking the beach all night with our red lights, buckets and FWC permit to get to where we are now. It took hundreds and thousands of more baby sea turtle hatchlings dying in streets, parking lots and dune vegetation before S.T.O.P. began to change
+      things. Before we found an amazing network of support and die-hard volunteers. Before we got the attention of FWC, FWS, the news media, national conservation groups, and the world. Before we got our first four infrared night vision cameras and caught in real time the nightly disorientation of
+      sea turtle hatchlings. Before using smartphones to compile volumes of never before collected, real-time data. <br />
+      But it took much longer than several summers. It took decades before someone said, on the most important loggerhead nesting beach in the world, "The endless, nightly slaughter of sea turtle hatchlings on Florida beaches has got to STOP!"
+    </p>
+  )
+  return (
+    <div className='home-section our-beginnings-section flex flex-col content-center justify-center items-center h-screen'>
+      <div className='our-beginnings-section-content flex flex-col justify-start items-start'>
+        <h2 className='our-beginnings-section-title'>Our Beginnings</h2>
+        <p className='our-beginnings-section-comment'>
+          Sea Turtle Oversight Protection (STOP) was founded in 2007 by the WhiteCloud Family, Richard, Siouxzen and their daughter Teakahla. <br />
+          <br />
+          STOP was the first program of its kind to rescue and release sea turtle hatchlings disoriented by coastal lighting in Broward County.
+          <br />
+          <br />
+          STOP now has the highest number of FWCC Marine Turtle Permitted staff of any organization in Florida!
+        </p>
+        <button className='action-button our-beginnings-button' onClick={() => setVisible(true)}>
+          Read More
+        </button>
+      </div>
+      {visible && <ContentModel title='How STOP Started' content={modelContent} setVisible={setVisible} />}
+    </div>
+  )
+}
+
+export default OurBeginning
